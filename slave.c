886065a198be513a73ff81fb5b8d0890f642a8cc
AxelCastroo <acastrobenza@itba.edu.ar>
2024-04-02 18:31:30 -0300

    Name change

--- slave.c
+++ slave.c
@@ -1,21 +1,21 @@
 #include "./includes/slave.h"
 
-int slaveProcess(int * masterToSlave, int * slaveToMaster){
+int slaveProcess(int * appToSlave, int * slaveToApp){
     
     int slaveToSlaveMaster[2];
-    char *params[] = {"/usr/bin/md5sum", NULL, NULL};
-    char * MyfileName;
+    char * params[] = {"/usr/bin/md5sum", NULL, NULL};
+    char * filename;
     char output[MD5_LENGTH + 1] = {0};
 
     while(1){
-        read(masterToSlave[STDIN_FD], &MyfileName, sizeof(char *));
+        read(appToSlave[STDIN_FD], &filename, sizeof(char *));
         createPipe(slaveToSlaveMaster);
         if(createSlave() == 0){
             closePipe(STDIN_FD);
             closePipe(slaveToSlaveMaster[STDIN_FD]);
             dupPipe(slaveToSlaveMaster[STDOUT_FD], STDOUT_FD);
             closePipe(slaveToSlaveMaster[STDOUT_FD]);
-            params[1] = MyfileName;
+            params[1] = filename;
             execvp("md5sum", params);
         }
         else{
@@ -24,7 +24,8 @@ int slaveProcess(int * masterToSlave, int * slaveToMaster){
             closePipe(slaveToSlaveMaster[STDIN_FD]);
             wait(NULL);
             read(STDIN_FD, output, MD5_LENGTH);
-            write(slaveToMaster[STDOUT_FD], output, MD5_LENGTH);        }
+            write(slaveToApp[STDOUT_FD], output, MD5_LENGTH);        
+            }
     }
     return 0;
 }
