50d94e193e093bf282a67f7a77b65e54a63b4bdd
LautiFarias <95657659+LautiFarias@users.noreply.github.com>
2024-04-02 17:08:09 -0300

    Update errors.h

---
+++ includes/errors.h
@@ -0,0 +1,13 @@
+#ifndef _ERRORS_H_
+#define _ERRORS_H_
+
+#define SEMAPHORE_ERROR 1
+#define SHMEM_ERROR 2
+#define SLAVE_ERROR 3
+#define PIPE_ERROR 4
+#define WRITE_ERROR 5
+#define READ_ERROR 6
+#define SELECT_ERROR 7
+#define FILE_ERROR 8
+
+#endif
