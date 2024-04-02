818917e9daec70606d20d3c853a9950b99752fea
LautiFarias <95657659+LautiFarias@users.noreply.github.com>
2024-04-02 16:58:03 -0300

    Create slave.h

---
+++ includes/slave.h
@@ -0,0 +1,20 @@
+#ifndef _SLAVE_H_
+#define _SLAVE_H_
+
+#include "utils.h"
+
+#define MD5_LENGTH 32
+#define STDIN_FD 0
+#define STDOUT_FD 1
+#define NUM_SLAVE_FILES 2
+
+typedef struct slave {
+    int master_to_slave_pipe[2];
+    int slave_to_master_pipe[2];
+    char *filename;
+    int process_id;
+} slave;
+
+int slaveProcess(int *app_to_slave_pipe, int *slave_to_app_pipe);
+
+#endif
