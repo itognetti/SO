37b48296e4dacc9293d96ac4cf36838565be4858
LautiFarias <95657659+LautiFarias@users.noreply.github.com>
2024-04-02 18:35:09 -0300

    Update slave.c

--- slave.c
+++ slave.c
@@ -2,26 +2,26 @@
 
 int slaveProcess(int * appToSlave, int * slaveToApp){
     
-    int slaveToSlaveMaster[2];
+    int SlaveCommunMaster[2];
     char * params[] = {"/usr/bin/md5sum", NULL, NULL};
     char * filename;
     char output[MD5_LENGTH + 1] = {0};
 
     while(1){
         read(appToSlave[STDIN_FD], &filename, sizeof(char *));
-        createPipe(slaveToSlaveMaster);
+        createPipe(SlaveCommunMaster);
         if(createSlave() == 0){
             closePipe(STDIN_FD);
-            closePipe(slaveToSlaveMaster[STDIN_FD]);
-            dupPipe(slaveToSlaveMaster[STDOUT_FD], STDOUT_FD);
-            closePipe(slaveToSlaveMaster[STDOUT_FD]);
+            closePipe(SlaveCommunMaster[STDIN_FD]);
+            dupPipe(SlaveCommunMaster[STDOUT_FD], STDOUT_FD);
+            closePipe(SlaveCommunMaster[STDOUT_FD]);
             params[1] = filename;
             execvp("md5sum", params);
         }
         else{
-            closePipe(slaveToSlaveMaster[STDOUT_FD]);
-            dupPipe(slaveToSlaveMaster[STDIN_FD], STDIN_FD);
-            closePipe(slaveToSlaveMaster[STDIN_FD]);
+            closePipe(SlaveCommunMaster[STDOUT_FD]);
+            dupPipe(SlaveCommunMaster[STDIN_FD], STDIN_FD);
+            closePipe(SlaveCommunMaster[STDIN_FD]);
             wait(NULL);
             read(STDIN_FD, output, MD5_LENGTH);
             write(slaveToApp[STDOUT_FD], output, MD5_LENGTH);        
