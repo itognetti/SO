f18ae11d77ccf86092ae98799ab69648b84fae00
AxelCastroo <acastrobenza@itba.edu.ar>
2024-04-02 17:02:37 -0300

    Added .h and started app,c

--- app.c
+++ app.c
@@ -1,5 +1,9 @@
 #include "./includes/app.h"
 
 int main(int argc, char * argv[]){
+    char * files[argc];
+    int filesCount = 0;
+    FILE * file = openFile("result.txt", "w");
+    
     return 0;
 }
---
+++ includes/lib.h
@@ -0,0 +1,18 @@
+#ifndef _LIB_H_
+#define _LIB_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "ipc.h"
+#include "slave.h"
+#include "errors.h"
+
+void error(char * message, int exitCode);
+
+#endif
